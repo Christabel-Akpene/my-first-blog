d9b5f53273e7fcbefd2fa361c9fcbb9411710d9c
Christabel-Akpene <dc.akpene@gmail.com>
2024-09-25 15:11:48 +0000

    first django commit

---
+++ hello.py
@@ -0,0 +1,20 @@
+print("Christabel")
+# print("Hi")
+
+name = "Ola"
+if name == "Ola":
+    print("Hello " + name)
+else:
+    print("Your name doesn't exist")
+
+
+def greetings(name):
+    print("Hello " + name )
+
+greetings("musafa")
+
+
+fruits = ["mango", "orange", "pineapple", "apple", "pineapple", "strawberry"]
+
+for fruit in fruits:
+    print("Hello, I am a " + fruit)
